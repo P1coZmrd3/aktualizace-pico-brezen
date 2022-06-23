f22e34357eb11d089d93f33d7b92a2b005f9e07c
Vlastnik <Vlastnik@DESKTOP-70EK70P>
2022-06-23 19:24:58 +0200

    Přidejte soubory projektu.

---
+++ aktualizace pico brezen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavidovaKuchyne               
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo1 = 1;
+            int cislo2 = 5;
+            int[] cisla = { cislo1, cislo2 };
+            Console.Clear();
+            int max = cisla.Max();
+            int min = cisla.Min();
+            Console.WriteLine(max + ", " + min);
+            
+            Console.ReadLine();
+        }
+    }
+}
